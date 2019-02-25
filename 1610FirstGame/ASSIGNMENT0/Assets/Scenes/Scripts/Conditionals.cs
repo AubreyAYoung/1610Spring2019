8fed21a29dd45fbeffed26f456be3f3febe8397d
Aubrey young <amillionflights@gmail.com>
2019-02-25 10:59:29 -0700

    If Then Else Statements

---
+++ 1610FirstGame/ASSIGNMENT0/Assets/Scenes/Scripts/Conditionals.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+
+
+	public bool CanWalk;
+	public int Number = 11;
+	public string Password; 
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update ()
+	 {
+
+		 if (CanWalk)
+		 {
+			 print("True");
+		 }
+		 else
+		 {
+			 print(false)
+		 }
+
+		 if (Number >= 10)
+		 {
+			 print("Greater");
+		 }
+		 else
+		 {
+			 print("Lesser")
+		 }
+
+		 if (Password == "OU812")
+		 {
+			 print("The password is correct");
+		 }
+		 else
+		 {
+			 print("The password is incorrect");
+		 }
+			 
+	}
+}
