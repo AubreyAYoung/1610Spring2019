9a12a5f4a6af9cb5f3be1129606d6b733332f75a
Aubrey young <amillionflights@gmail.com>
2019-02-27 11:11:30 -0700

    Created Character Controller

---
+++ 1610FirstGame/ASSIGNMENT0/Assets/Scenes/Scripts/MoveCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveCharacter : MonoBehaviour
+{
+	public UnityEvent OnGrounded, OffGrounded; 
+	
+	public float Speed = 3;
+	
+	
+	private CharacterController _controller;
+	private Vector3 position;
+	
+	private void Start()
+	{
+		_controller = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		if (_controller.isGrounded)
+		{
+			OnGrounded.Invoke();
+			
+		}
+		else
+		{
+			OffGrounded.Invoke();
+		}
+		
+		position.y = Input.GetAxis("Vertical")*Speed*Time.deltaTime;
+		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime; 
+		_controller.Move(position);
+	}
+}
