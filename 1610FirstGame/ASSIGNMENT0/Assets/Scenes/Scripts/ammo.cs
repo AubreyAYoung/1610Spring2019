c2a4013aa13353f47b223bccd259462da39e6306
Aubrey young <amillionflights@gmail.com>
2019-03-01 20:59:16 -0700

    made a script for ammo pickup

---
+++ 1610FirstGame/ASSIGNMENT0/Assets/Scenes/Scripts/ammo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammo : PowerUpBase 
+{
+
+	public float AmmoValue = 3f;
+	
+	public void OnAmmoPickup (float AddAmmoValue)
+	{
+		Value += AddAmmoValue;
+	}
+	
+}
