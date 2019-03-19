33bee4cf73dc784ae858785975bf6ba2843f437b
Aubrey young <amillionflights@gmail.com>
2019-03-19 17:02:14 -0600

    game redo day1

---
+++ 1610FirstGame/Little Greek Life/Assets/Scripts/AnimalClasses/Animal1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.Events; 
+
+public class Animal1 : MonoBehaviour
+{
+
+	public string Name;
+	public FloatData1 Health;
+	public FloatData1 Speed;
+	public bool CanMove;
+	public int Energy = 100; 
+	public Color SkinColor; 
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1610FirstGame/Little Greek Life/Assets/Scripts/AnimalClasses/Doggo1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.Events; 
+
+public class Doggo1 : Mammal1 
+{
+
+    public int Hunger = 50;
+    public float HungerIncrease = 0.5f;
+	
+    // Use this for initialization
+    void Start () 
+    {
+        print("Hello, World"); 
+        GetComponent<SpriteRenderer>().color = SkinColor; 		
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Rotate(0,1,0);	
+    }
+    
+}
