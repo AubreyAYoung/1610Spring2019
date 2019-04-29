8b67bafcab57bbdc96e05c2a5be1a07013fe61cc
Aubrey Young <amillionflights @gmail.com>
2019-04-29 11:03:06 -0600

    switch statement

---
+++ ASSIGNMENT0/Assets/Scenes/Scripts/Greetings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Greetings : MonoBehaviour
+{
+//got this from a tutorial
+	public int GreetingNumber = 5;
+
+	void Greet()
+	{
+		switch (GreetingNumber)
+		{
+			
+			case 5:
+				print("Henlo");
+				break;
+			case 4:
+				print("Welcome to Little Greek World");
+				break; 
+			case 3:
+				print("Press start to begin");
+				break; 
+			case 2:
+				print("Press anywhere to begin");
+				break; 
+			case 1:
+				print("oof sorry this is print statement");
+				break;
+			default:
+				print ("Welcome Player");
+				break;
+			
+		}
+		
+		
+	}
+
+}
